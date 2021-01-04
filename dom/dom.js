fecb0a81211e16d65d63cd66e5c07b49ec15bf8d
alexangria1992 <alex040892@gmail.com>
2021-01-05 12:54:15 +1300

    Selecting in the DOM

---
+++ dom/dom.js
@@ -0,0 +1,8 @@
+// const headers = document.getElementsByTagName("h2")
+// const list = document.getElementsByClassName("item")
+// const button = document.getElementById("submit")
+
+const headers = document.querySelectorAll("h2")
+const list = document.querySelector(".item")
+
+console.log(list)
